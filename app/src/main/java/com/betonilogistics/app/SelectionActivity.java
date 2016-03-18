9ad695c80e5eee7d4b317d2ec64625336768fab8
hackaton <ponomarev45@gmail.com>
2016-03-18 23:58:22 +0200

    Selection activity changes

--- app/src/main/java/com/betonilogistics/app/SelectionActivity.java
+++ app/src/main/java/com/betonilogistics/app/SelectionActivity.java
@@ -64,7 +64,7 @@ public class SelectionActivity extends AppCompatActivity{
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_startup, menu);
+        getMenuInflater().inflate(R.menu.selection, menu);
         return true;
     }
 
